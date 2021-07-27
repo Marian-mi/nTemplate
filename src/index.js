ed26cea80778ff55808a5b7c35e083c12bd25727
mahdi <mahdi.maryan@yahoo.com>
2021-07-27 17:16:53 +0430

    initial commit

---
+++ src/index.js
@@ -0,0 +1,10 @@
+
+$('.slick-slider').slick({
+    arrows: true,
+    dots: true,
+    prevArrow: '<button class="slicks-next slicks-arrow"><i class="fas fa-arrow-right"></i></button>',
+    nextArrow: '<button class="slicks-prev slicks-arrow"><i class="fas fa-arrow-left"></i></button >',
+})
+
+
+    
