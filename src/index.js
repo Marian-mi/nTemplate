91a20ac20f6ba230c619284e95ea6a0a323a7c65
mahdi <mahdi.maryan@yahoo.com>
2021-07-28 09:47:46 +0430

    Added courses section

--- src/index.js
+++ src/index.js
@@ -2,9 +2,38 @@
 $('.slick-slider').slick({
     arrows: true,
     dots: true,
-    prevArrow: '<button class="slicks-next slicks-arrow"><i class="fas fa-arrow-right"></i></button>',
-    nextArrow: '<button class="slicks-prev slicks-arrow"><i class="fas fa-arrow-left"></i></button >',
+    prevArrow: '<button class="slicks-prev slicks-arrow"><i class="fas fa-arrow-left"></i></button>',
+    nextArrow: '<button class="slicks-next slicks-arrow"><i class="fas fa-arrow-right"></i></button >',
 })
+
+$('.courses-slider').slick({
+    dots: true,
+    infinite: false,
+    speed: 300,
+    slidesToShow: 3,
+    slidesToScroll: 3,
+    adaptiveHeight: true,
+    prevArrow: '<button class="courses-prev courses-arrow"><i class="fas fa-arrow-left"></i></button>',
+    nextArrow: '<button class="courses-next courses-arrow"><i class="fas fa-arrow-right"></i></button >',
+    responsive: [
+      {
+        breakpoint: 1350,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2,
+          infinite: true,
+          dots: true
+        }
+      },
+      {
+        breakpoint: 856,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1
+        }
+      },
+    ]
+  });
 
 
     
